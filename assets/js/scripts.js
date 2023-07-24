f92467ac9f801ac96c2341f2ea191e875dbf2424
Gunay Zahra <126316477+shzehra93@users.noreply.github.com>
2023-07-24 16:08:51 +0400

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,40 +1,14 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const sloganSection = document.querySelector('.slogan');
+$(document).ready(function() {
+    $('.gallery img').click(function() {
+      var src = $(this).attr('src');
+      var alt = $(this).attr('alt');
 
-//     sloganSection.addEventListener('click', function() {
-//         // Randomly generate a hex color code
-//         const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+      var largeImage = $('<img>').attr('src', src).attr('alt', alt);
+      var overlay = $('<div>').addClass('overlay').append(largeImage);
+      $('body').append(overlay);
 
-//         // Change the background color of the slogan section
-//         sloganSection.style.backgroundColor = randomColor;
-//     });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const galleryImages = document.querySelectorAll('.gallery img');
-//     const lightbox = document.createElement('div');
-//     lightbox.classList.add('lightbox');
-
-//     galleryImages.forEach(image => {
-//       image.addEventListener('click', function() {
-//         const imageClone = image.cloneNode();
-//         lightbox.innerHTML = '';
-//         lightbox.appendChild(imageClone);
-//         lightbox.style.display = 'flex';
-//       });
-//     });
-
-//     lightbox.addEventListener('click', function() {
-//       lightbox.style.display = 'none';
-//     });
-
-//     document.body.appendChild(lightbox);
-//   });
-
-const tableRows = document.querySelectorAll('.table tbody tr');
-
-    tableRows.forEach((row) => {
-      row.addEventListener('click', () => {
-        alert(`You clicked on row ${row.cells[0].innerText}`);
+      overlay.click(function() {
+        overlay.remove();
       });
     });
+  });
